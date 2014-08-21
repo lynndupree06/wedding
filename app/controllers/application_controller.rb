ff51447814b2c99486181b8401d8905525a8b791
Jessica Brazelton <jbrazelt@thoughtworks.com>
2014-08-21 18:16:56 +1000

    remove exception notification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,31 +4,31 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   http_basic_authenticate_with name: 'jfk', password: '*$t@r8@r'
 
-  unless Rails.application.config.consider_all_requests_local
-	  rescue_from Exception,
-	              :with => :render_error
-	  rescue_from ActiveRecord::RecordNotFound,
-	              :with => :render_not_found
-	  rescue_from ActionController::RoutingError,
-	              :with => :render_not_found
-	  rescue_from ActionController::UnknownController,
-	              :with => :render_not_found
-	  rescue_from ActionController::UnknownAction,
-	              :with => :render_not_found
-	end
+#   unless Rails.application.config.consider_all_requests_local
+# 	  rescue_from Exception,
+# 	              :with => :render_error
+# 	  rescue_from ActiveRecord::RecordNotFound,
+# 	              :with => :render_not_found
+# 	  rescue_from ActionController::RoutingError,
+# 	              :with => :render_not_found
+# 	  rescue_from ActionController::UnknownController,
+# 	              :with => :render_not_found
+# 	  rescue_from ActionController::UnknownAction,
+# 	              :with => :render_not_found
+# 	end
 
-  protected
+#   protected
 
-  def render_not_found(exception)
-  render :template => "/errors/404.html.erb",
-         :layout => 'errors.html.erb'
-end
+#   def render_not_found(exception)
+#   render :template => "/errors/404.html.erb",
+#          :layout => 'errors.html.erb'
+# end
  
-def render_error(exception)
-  ExceptionNotifier::Notifier
-    .exception_notification(request.env, exception)
-    .deliver
-  render :template => "/errors/500.html.erb",
-         :layout => 'errors.html.erb'
-end
+# def render_error(exception)
+#   ExceptionNotifier::Notifier
+#     .exception_notification(request.env, exception)
+#     .deliver
+#   render :template => "/errors/500.html.erb",
+#          :layout => 'errors.html.erb'
+# end
 end
