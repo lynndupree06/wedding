40cf033c55cb05eb90799b9a3f6791ae27b08a55
Jessica Brazelton <jbrazelt@thoughtworks.com>
2014-08-26 10:29:13 +1000

    updated party notifier and changed query in guest summary

--- app/mailers/party_notifier.rb
+++ app/mailers/party_notifier.rb
@@ -7,6 +7,8 @@ class PartyNotifier < ActionMailer::Base
       @token = PartyEncoder.encode(@party.id)
       mail(:to => party.email,
            :subject => 'Save the Date! July 25, 2015')
+      party.save_the_date_sent = true
+      party.save!
     end
   end
 end
