c2353043bc21f4d769d0248b667f6fd0eb6b163c
Jessica <jbrazelt@thoughtworks.com>
2014-10-08 13:18:52 +1100

    Added photos of fred and jess on our story. Added new bridesmaid photo. Made some UI changes.

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,13 +1,3 @@
 $(function () {
-  // $.ajax({
-  //  url: 'http://trycapsule.com/c/198568-jessica-frederick-wedding/photos'
-  // }).done(function(data) { // data variable contains fetched html
-  //  var parsedDom = data;
-  //
-  //  var images = data.match(/(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$(jpeg|jpg)/g);
-  //
-  //  // check if it works
-  //  console.log( '[TEST] item price: ' );
-  //  console.log( images );
-  // });
+  var $images_dir = 'engagement-photos';
 });
