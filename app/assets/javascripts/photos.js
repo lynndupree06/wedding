9505c2a2ed2834d0274857c70366788db95ca525
Jessica <jbrazelt@thoughtworks.com>
2014-10-11 13:28:31 +1100

    Added tab pills to photo page.

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,6 +1,6 @@
-$(function() {
+$(function () {
   $("img.lazy").show().lazyload({
-    effect : "fadeIn"
+    effect: "fadeIn"
   });
 
   $('img.lazy').click(function () {
@@ -10,5 +10,10 @@ $(function() {
 
   $('.carousel').carousel({
     interval: false
+  });
+
+  $('.tabs a').click(function () {
+    $('.tabs a').removeClass('active');
+    $(this).addClass('active');
   });
 });
