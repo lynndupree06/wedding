a2d23e61e4927ddb1d661f6ab26fc7b0d317b371
Jessica <jbrazelt@thoughtworks.com>
2014-10-21 22:44:36 +1100

    Added heading to photo modal.

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -4,12 +4,34 @@ $(function () {
   });
 
   $('img.lazy').click(function () {
-    $('#engagement-' + $(this).attr('id')).addClass('active');
+    var id = $(this).attr('id');
+    $('.item').removeClass('active');
+    $('#engagement-' + id).addClass('active');
     $('#myModal').modal('show');
+    setModalTitle(Number(id) + 1);
   });
 
   $('.carousel').carousel({
     interval: false
+  });
+
+  function setModalTitle(id) {
+    $('.modal-title').html('Engagement Photos: ' + id + ' of 17');
+  }
+
+  $('.carousel-control').click(function () {
+    var right = $(this).attr('class').indexOf('right') >= 0;
+    var id = $(this).parent().find('.active').attr('id');
+    var idNumber = Number(id.substr(id.indexOf('-') + 1)) + 1;
+
+    idNumber = right ? idNumber + 1 : idNumber - 1;
+
+    switch (idNumber) {
+      case 0: idNumber = 17; break;
+      case 18: idNumber = 1; break;
+    }
+
+    setModalTitle(idNumber);
   });
 
   $('.tabs a').click(function () {
