2bf72bf65593bb427e527ef0c316cb5ef764ac87
Jessica Brazelton <jbrazelt@thoughtworks.com>
2015-06-04 21:10:55 -0500

    Fixed rsvp query

--- app/helpers/rsvp_helper.rb
+++ app/helpers/rsvp_helper.rb
@@ -1,5 +1,5 @@
 module RsvpHelper
 	def party_options
-		Party.order(:name).where('rsvp != ? OR rsvp != ? OR rsvp = 0 OR rsvp IS NULL', true, 't')
+		Party.order(:name).where('rsvp != ? OR rsvp IS NULL', true)
 	end
 end
